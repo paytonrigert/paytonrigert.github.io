75a4d17c85978fee1e52d8c690668f3c7f14de1e
CarterRigert <89533142+CarterRigert@users.noreply.github.com>
2023-04-14 19:03:09 -0600

    Fixed NavBAR

--- nova/src/components/Navbar.js
+++ nova/src/components/Navbar.js
@@ -2,18 +2,21 @@ import React from 'react';
 import './Css/Navbar.css';
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import nova from '../images/NovaPHC.png';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark">
             <div className="container">
-                
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
                 <a className="navbar-brand" href="#">
                     <img className="navbar-logo" src={nova} alt="Nova" />
-                    <a> </a>
-                     Nova Public Health Consulting
+                    Nova Public Health Consulting
                 </a>
-                <div className="collapse navbar-collapse">
+                <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav ms-auto">
                         <li className="nav-item">
                             <Link className="nav-link" to="/">Home</Link>
@@ -21,7 +24,6 @@ const Navbar = () => {
                         <li className="nav-item">
                             <Link className="nav-link" to="team">Team</Link>
                         </li>
-                       
                         <li className="nav-item">
                             <a className="nav-link" href="#contact">
                                 Contact
